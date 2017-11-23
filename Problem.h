8d9414ef5354b3a243d99de28379f83d35b24099
Tarek-B <tarek.belgacem@uha.fr>
2017-11-23 10:39:49 +0100

    Create Problem.h

---
+++ Problem.h
@@ -0,0 +1,22 @@
+ class Problem
+  {
+	public:
+		Problem();
+		~Problem();
+
+		friend ostream& operator<< (ostream& os, const Problem& pbm);
+		friend istream& operator>> (istream& is, Problem& pbm);
+
+		Problem& operator=  (const Problem& pbm);
+		bool operator== (const Problem& pbm) const;
+		bool operator!= (const Problem& pbm) const;
+
+		//Direction direction () const; //Maximize or Minimize (effacée par le prof)
+		int dimension() const;		
+		double LowerLimit, UpperLimit;
+
+	private:
+
+		int _dimension;
+	  	int _numf; // le num d'une fonction (rosenbrock etc. )
+  };
