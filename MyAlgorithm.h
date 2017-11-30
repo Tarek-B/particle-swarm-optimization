a962c2e94eac772a709fee78dcccacaf770ad1fa
Lydii07 <33064237+Lydii07@users.noreply.github.com>
2017-11-30 11:09:35 +0100

    Update MyAlgorithm.h

--- MyAlgorithm.h
+++ MyAlgorithm.h
@@ -7,23 +7,20 @@
 #define MYALGORITHM_H
 #ifndef MYALGORITHM_H
 
-/*
-	Class representing our program
-**/
+
 
 class MyAlgorithm
   {
 	private:
-		// individuals in population
+	
 		vector<Solution*> d_solutions;   
-		// A problem
-		const Problem& d_pbm;
+
 		// Parameters
 		const SetUpParams& d_setup;
 		// Upper fitness of individuals in population
 		unsigned int d_upper_cost; 
- 		//d_lower_cost; lower fitness of individuals in population
- 		//vector<struct particle> _fitness_values;
+ 		unsigned intd_lower_cost; lower fitness of individuals in population
+ 		vector<struct particle> _fitness_values;
 	public:
 		/*
 		  Constructor with a problem and parameters
