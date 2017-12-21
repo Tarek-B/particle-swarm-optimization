e70418130eee73b2d2400f59e6f9e842bc8350c4
esoz1 <33064218+esoz1@users.noreply.github.com>
2017-12-21 13:47:26 +0100

    Add files via upload

--- SetUpParams.cpp
+++ SetUpParams.cpp
@@ -1,65 +1,62 @@
+#include <iostream>
+
 #include "SetUpParams.h"
  
 
 /** Builder
 	
 */
-SetUpParams::SetUpParams ():  d_independent_runs{30}, d_nb_evolution_steps{2e6/30}, d_population_size{30}, d_solution_size {30}
+SetUpParams::SetUpParams ():  d_independent_runs{30}, d_nb_evolution_steps{66667}, d_population_size{30}, d_solution_size {30}
 {}
 /** Destructive 
-
 */
 SetUpParams::~SetUpParams()
 {}
 /** Turn the number of launches
 	@return d_independent_runs - The number of independent launches
 */
-const unsigned int   SetUpParams::independent_runs() const
+int   SetUpParams::independent_runs() const
 {
 	return d_independent_runs;
 }
 /** Turn the number of iterations per run
 	@return d_nb_evolution_steps - the number of iterations per run
 */
-const unsigned int   SetUpParams::nb_evolution_steps() const
+int   SetUpParams::nb_evolution_steps() const
 {
 	return d_nb_evolution_steps;
 }
 /** Turn the size of the population
 	@return d_population_size - the size of the population
-
 */
-const unsigned int   SetUpParams::population_size() const
+int   SetUpParams::population_size() const
 {
 	return d_population_size;
 }
-const unsigned int   solution_size() const
+int   solution_size() const
 { 
 	return d_solution_size ;
 }
 /**Modify the number of launches
 	@param[in] d_independent_runs - the number of launches
-
 */
 
-void SetUpParams::independent_runs(const unsigned int val)
+void SetUpParams::independent_runs(int val)
 {
 	 d_independent_runs=val;
 }
 
 /**Modify the number of iterations per run
 	@param[in] d_nb_evolution_steps - the number of iterations per run
-
 */
-void SetUpParams::nb_evolution_steps(const unsigned int val)
+void SetUpParams::nb_evolution_steps(int val)
 {
 	d_nb_evolution_steps=val;
 }
 /**Modify the size of the population
 	@param[in] d_population_size - the size of the population
-
 */
-void SetUpParams::population_size(const unsigned int val)
+void SetUpParams::population_size(int val)
 {
 	d_population_size=val;
 }
@@ -73,8 +70,6 @@ void solution_size(const unsigned int val)
 	@param[in, out] is - the output stream
 	@param[in] setup - the parameters of the problem
 	@return is - the output stream
-
-
 */
 std::ostream& operator<< (std::ostream& os, const SetUpParams& setup)
 {
@@ -87,7 +82,6 @@ std::ostream& operator<< (std::ostream& os, const SetUpParams& setup)
 	@param[in, out] is - The input stram 
 	@param[in] setup - the parameters of the problem
 	@return is - the input stream
-
 */
 std::istream& operator>> (std::istream& is, SetUpParams& setup)
 {
@@ -105,7 +99,4 @@ std::istream& operator>> (std::istream& is, SetUpParams& setup)
 	}
 	return is;	
 }
-
-
-
 
