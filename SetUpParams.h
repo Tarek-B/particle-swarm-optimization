b75335a18c3d715f35a2c6b45e8e52cd83ef5499
esoz1 <33064218+esoz1@users.noreply.github.com>
2017-12-21 13:47:36 +0100

    Add files via upload

--- SetUpParams.h
+++ SetUpParams.h
@@ -20,40 +20,31 @@ class SetUpParams
 			@param[in] setup - the parameters of the problem
 			@return is - the output stream
 		*/
-		friend ostream& operator<< (ostream& os, const SetUpParams& setup);
-		/** input operator 
-			@param[in, out] is - The input stram 
-			@param[in] setup - the parameters of the problem
-			@return is - the input stream
-		*/
-		friend istream& operator>> (istream& is, SetUpParams& setup);
-		/** Turn the number of launches
-			@return d_independent_runs - The number of independent launches
-		*/	
-		const unsigned int   independent_runs() const;
+			
+	    int   independent_runs() const;
 		/** Turn the number of iterations per run
 			@return d_nb_evolution_steps - the number of iterations per run
 		*/	
-		const unsigned int   nb_evolution_steps() const; 
+		int   nb_evolution_steps() const; 
 		/** Turn the size of the population
 			@return d_population_size - the size of the population
 		*/
-		const unsigned int   population_size() const;
-		const unsigned int   solution_size() const;
+		int   population_size() const;
+		int   solution_size() const;
 		/**Modify the number of launches
 			@param[in] d_independent_runs - the number of launches
 		*/
-		void independent_runs(const unsigned int val);
+		void independent_runs(const int val);
 		/**Modify the number of iterations per run
 			@param[in] d_nb_evolution_steps - the number of iterations per run
 		*/
-		void nb_evolution_steps(const unsigned int val);
+		void nb_evolution_steps(const int val);
 		/**Modify the size of the population
 			@param[in] d_population_size - the size of the population
 		*/
-		void population_size(const unsigned int val);
+		void population_size(const int val);
 		
-		void solution_size(const unsigned int val);
+		void solution_size(const int val);
 
 		~SetUpParams();
 };
